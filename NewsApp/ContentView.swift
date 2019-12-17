353a63091a73776dbf238fe702ad5a11d0a77c97
Tim DiLauro <tim.di.lauro@gmail.com>
2019-12-16 19:59:23 -0500

    Initial Commit

---
+++ NewsApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  NewsApp
+//
+//  Created by Tim DiLauro on 12/16/19.
+//  Copyright © 2019 Five Lions. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
