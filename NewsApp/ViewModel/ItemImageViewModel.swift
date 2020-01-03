7f4f9f3ae10eaeb2a0137a5295cd9d2529b1af73
Tim DiLauro <tim.di.lauro@gmail.com>
2020-01-02 22:18:02 -0500

    default/broken link feed images

--- NewsApp/ViewModel/ItemImageViewModel.swift
+++ NewsApp/ViewModel/ItemImageViewModel.swift
@@ -13,13 +13,19 @@ import Combine
 class ItemImageViewModel: ObservableObject {
 
     @Published var image: UIImage = UIImage()
-    
+
+    static private let defaultFeedImage = UIImage(named: "Default Feed Icon")!
+    static private let brokenLinkImage = UIImage(named: "Broken Link Icon")!
+
     private var cancellable: AnyCancellable?
 
     init(imageUrl: String?) {
+        image = Self.defaultFeedImage
+
         guard let url = imageUrl else { return }
 
         cancellable = UIImage.publish(downloadedFrom: url)
+            .map { $0 == nil ? Self.brokenLinkImage : $0 }
             .receive(on: RunLoop.main)
             .sink(receiveValue: { image in
                 guard let image = image else { return }
